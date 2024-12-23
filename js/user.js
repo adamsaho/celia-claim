5b006d1766d607c426c997d7ffaf2a99d8dc0f28
0xtarit <0xtarit@gmail.com>
2024-12-23 10:41:55 +0530

    some changes

--- js/user.js
+++ js/user.js
@@ -1129,7 +1129,7 @@ let users = [
         '0x40ef00a18b34a22275b0718f308ea142a806f09c0ef81a96617d14a8b6c3f2d7',
       ],
       user: '0xd9ff067d0350fe2bb219c8cab3d457315beb5244',
-      status: 'pending',
+      status: 'claimed',
     },
   },
 
